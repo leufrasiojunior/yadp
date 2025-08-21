6e093db412dc17a56515df0e5ada900a5868c024
Leonald <leufrasiojunior@gmail.com>
2025-08-21 13:25:11 -0300

    refatorar componentes de cartões no painel para melhorar a exibição de consultas DNS; atualizar traduções para incluir descrições mais detalhadas

--- src/app/[locale]/(main)/dashboard/default/_components/section-cards.tsx
+++ src/app/[locale]/(main)/dashboard/default/_components/section-cards.tsx
@@ -3,7 +3,7 @@
 import { useEffect } from "react";
 
 import { TooltipTrigger } from "@radix-ui/react-tooltip";
-import { CircleQuestionMark, Globe } from "lucide-react";
+import { Ban, CircleQuestionMark, Globe, Percent } from "lucide-react";
 import { useTranslations } from "next-intl";
 
 import { Badge } from "@/components/ui/badge";
@@ -59,7 +59,7 @@ export function SectionCards() {
   return (
     <div className=":data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
       <Card className="@container/card">
-        <div className="flex h-full items-center gap-4 p-4">
+        <div className="flex h-full items-center gap-4 px-4">
           <div className="flex h-20 w-20 items-center justify-center">
             <Globe className="h-12 w-12" />
           </div>
@@ -78,45 +78,80 @@ export function SectionCards() {
             <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
               {summary?.queries.total.toLocaleString()}
             </CardTitle>
-            <CardFooter className="text-muted-foreground mt-1 p-0 text-sm">{t("queriesTotal.description")}</CardFooter>
           </div>
         </div>
       </Card>
 
       <Card className="@container/card">
-        <CardHeader>
-          <CardDescription>{t("queriesBlocked.title")}</CardDescription>
-          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            {summary?.queries.blocked.toLocaleString()}
-          </CardTitle>
-        </CardHeader>
-        <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="text-muted-foreground">{t("queriesBlocked.description")}</div>
-        </CardFooter>
+        <div className="flex h-full items-center gap-4 p-4">
+          <div className="flex h-20 w-20 items-center justify-center">
+            <Ban className="h-12 w-12" />
+          </div>
+          <div className="flex flex-1 flex-col justify-center">
+            <div className="flex items-center justify-between">
+              <CardDescription>{t("queriesBlocked.title")}</CardDescription>
+              <Badge variant="secondary" className="h-6 bg-transparent px-2">
+                <Tooltip>
+                  <TooltipTrigger>
+                    <CircleQuestionMark className="h-4 w-4" />
+                  </TooltipTrigger>
+                  <TooltipContent>{t("queriesBlocked.description")}</TooltipContent>
+                </Tooltip>
+              </Badge>
+            </div>
+            <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+              {summary?.queries.blocked.toLocaleString()}
+            </CardTitle>
+          </div>
+        </div>
       </Card>
 
       <Card className="@container/card">
-        <CardHeader>
-          <CardDescription>{t("queriesPercent.title")}</CardDescription>
-          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            {summary?.queries.percent_blocked.toFixed(2)}%
-          </CardTitle>
-        </CardHeader>
-        <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="text-muted-foreground">{t("queriesPercent.description")}</div>
-        </CardFooter>
+        <div className="flex h-full items-center gap-4 p-4">
+          <div className="flex h-20 w-20 items-center justify-center">
+            <Percent className="h-12 w-12" />
+          </div>
+          <div className="flex flex-1 flex-col justify-center">
+            <div className="flex items-center justify-between">
+              <CardDescription>{t("queriesPercent.title")}</CardDescription>
+              <Badge variant="secondary" className="h-6 bg-transparent px-2">
+                <Tooltip>
+                  <TooltipTrigger>
+                    <CircleQuestionMark className="h-4 w-4" />
+                  </TooltipTrigger>
+                  <TooltipContent>{t("queriesPercent.description")}</TooltipContent>
+                </Tooltip>
+              </Badge>
+            </div>
+            <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+              {summary?.queries.percent_blocked.toFixed(2)}%
+            </CardTitle>
+          </div>
+        </div>
       </Card>
 
       <Card className="@container/card">
-        <CardHeader>
-          <CardDescription>{t("queriesPercent.title")}</CardDescription>
-          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            {summary?.gravity.domains_being_blocked.toLocaleString()}
-          </CardTitle>
-        </CardHeader>
-        <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="text-muted-foreground">{t("queriesPercent.description")}</div>
-        </CardFooter>
+        <div className="flex h-full items-center gap-4 p-4">
+          <div className="flex h-20 w-20 items-center justify-center">
+            <Percent className="h-12 w-12" />
+          </div>
+          <div className="flex flex-1 flex-col justify-center">
+            <div className="flex items-center justify-between">
+              <CardDescription>{t("totalDomains.title")}</CardDescription>
+              <Badge variant="secondary" className="h-6 bg-transparent px-2">
+                <Tooltip>
+                  <TooltipTrigger>
+                    <CircleQuestionMark className="h-4 w-4" />
+                  </TooltipTrigger>
+                  <TooltipContent>{t("totalDomains.description")}</TooltipContent>
+                </Tooltip>
+              </Badge>
+            </div>
+            <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+              {summary?.gravity.domains_being_blocked.toLocaleString()}
+            </CardTitle>
+          </div>
+        </div>
       </Card>
     </div>
   );
